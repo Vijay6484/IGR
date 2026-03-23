bee1edb2c6ee4f2aa885c24db22d2e2a5c93f602
Vijay Tiwari <vijaytiwari@Vijays-MacBook-Air.local>
2026-03-23 17:46:44 +0530

    sng

--- run_1py_villages_tmux_parallel.sh
+++ run_1py_villages_tmux_parallel.sh
@@ -16,7 +16,7 @@ DISTRICT_INDEX="1"
 TEHSIL_INDEX="12"
 
 # Village dropdown index list (1-based). One tmux session is created per village index.
-VILLAGE_INDICES=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 50 51 52 53 54 55 56 57 58 59 60 61 )
+VILLAGE_INDICES=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 )
 # -------------------------------
 
 # Optional: prefer project venv if present
